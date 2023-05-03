471444234e3c9f77ba2e30cf5ce33f3976c29204
Crisley Venâncio Marques <crisley.marques@ccc.ufcg.edu.br>
2023-05-03 15:12:28 -0300

    Add q2 from week 7

--- week-7/q_2.sql
+++ week-7/q_2.sql
@@ -6,3 +6,20 @@
 -- Use um laço LOOP e a opção %ROWTYPE. 
 -- Execute o bloco PL/SQL. Copie e cole o código produzido, bem como o resultado da execução.
 
+DECLARE
+    CURSOR c_depts IS 
+      SELECT DEPARTMENT_NAME, COUNT(EMPLOYEE_ID) as qtd_emp
+      FROM OEHR_EMPLOYEES e, OEHR_DEPARTMENTS d 
+      WHERE e.DEPARTMENT_ID = d.DEPARTMENT_ID 
+      GROUP BY DEPARTMENT_NAME
+      HAVING count(e.employee_id) > 5; 
+    v_depts c_depts%ROWTYPE;
+BEGIN
+    OPEN c_depts;
+    LOOP
+        FETCH c_depts INTO v_depts;
+        EXIT WHEN c_depts%NOTFOUND;
+        DBMS_OUTPUT.PUT_LINE(c_depts%ROWCOUNT || '. ' || v_depts.department_name || ' - ' || v_depts.qtd_emp || ' funcionários');
+    END LOOP;
+    CLOSE c_depts;
+END;
